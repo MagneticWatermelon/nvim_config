d9ae903670dd58136bc203a0807e6568cfad3860
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-03-31 22:57:38 +0300

    change default config

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,18 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  {
+    "rose-pine",
+    opts = {
+      disable_italics = true,
+    },
+  },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
 }
